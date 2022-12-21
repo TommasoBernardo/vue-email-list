54d81ab4ac57aea40114093df73c151e58e61035
TommasoBernardo <tommasob1210@gmail.com>
2022-12-21 15:59:19 +0100

    create function and create ciclo for

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
 let app = new Vue({
-    
+    el : '#container',
+    data: {
+        email:{},
+
+        emails: [],
+    },
+
+    getRandomEmail(){
+        const cont = this;
+
+        for( let i = 0 ; i < 10 ; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(function(generatedEmail){
+                cont = email =
+            })
+        }
+    }
 })
